112c4cf9efa87f312d21a46b5af42a582910eef0
Alexander Gitskiy <zhitskiy1991@gmail.com>
2014-10-24 23:04:34 +0300

    add modal window factory

--- app/client/scripts/controllers/main_controller.js
+++ app/client/scripts/controllers/main_controller.js
@@ -1,28 +1,10 @@
 'use strict';
 
 angular.module('Expertise').
-    controller('mainController', function ($rootScope, $scope, $http, $location, $modal, SignupModalCtrl, LoginModalCtrl) {
+    controller('mainController', function ($rootScope, $scope, ModalWindowFactory, SignupModalCtrl, LoginModalCtrl) {
 
-      $rootScope.loginModelOpen = function () {
-        var modalInstance = $modal.open({
-          templateUrl: '../views/login_modal.html',
-          controller: LoginModalCtrl
-        });
-      };
-
-      $rootScope.signupModelOpen = function () {
-        var modalInstance = $modal.open({
-          templateUrl: '../views/signup_modal.html',
-          controller: SignupModalCtrl
-        });
-      };
-
-      $rootScope.logout = function () {
-        $http.get('/logout').success(function (res) {
-          delete $rootScope.user;
-          $location.path("/welcome");
-        });
-      };
+      $rootScope.loginModelOpen = ModalWindowFactory('login', LoginModalCtrl);
+      $rootScope.signupModelOpen = ModalWindowFactory('signup', SignupModalCtrl);
 
     });
 
