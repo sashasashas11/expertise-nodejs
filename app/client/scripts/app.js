f237e9c475d7a5e95cc04b4ba203fe767f1c27d5
Alexander Gitskiy <zhitskiy1991@gmail.com>
2014-10-29 22:51:04 +0200

    add material design lib

--- app/client/scripts/app.js
+++ app/client/scripts/app.js
@@ -5,14 +5,19 @@ angular.module('Expertise', [
 		'ui.bootstrap',
     'ngResource',
 		'textAngular',
-		'ui'
+		'ui',
+//    'ngAnimate',
+//    'ngAria',
+		'ngMaterial'
 	])
-  .config(function ($routeProvider) {
+  .config(function ($routeProvider, $locationProvider) {
 		$routeProvider.
 			when("/welcome", { templateUrl: "views/welcome.html", controller: "welcomeController" }).
 			when("/expertise", { templateUrl: "views/expertise.html", controller: "expertiseController" }).
 			otherwise({redirectTo: '/welcome'});
+
+      $locationProvider.html5Mode(true);
 	})
-	.config(['$locationProvider', function ($locationProvider) {
-		$locationProvider.html5Mode(true).hashPrefix('!');
-	}]);
+//	.config(['$locationProvider', function ($locationProvider) {
+//		$locationProvider.html5Mode(true).hashPrefix('!');
+//	}]);
--- app/client/scripts/controllers/welcome_controller.js
+++ app/client/scripts/controllers/welcome_controller.js
@@ -3,6 +3,7 @@
 angular.module('Expertise').
 	controller('welcomeController', function ($rootScope, $scope, $http, MethodsService) {
 		$scope.methods = MethodsService.query();
+    $scope.editText = [];
 
 		$scope.updateMethod = function(method) {
       MethodsService.update({id: method._id}, { text: method.text });
